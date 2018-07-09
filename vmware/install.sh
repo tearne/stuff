69464d2f26bb205a56b24a9cd23b804f714f9ace
tearne <tearne@gmail.com>
2018-07-09 20:54:17 +0100

    Updates for ESXi

--- vmware/install.sh
+++ vmware/install.sh
@@ -8,12 +8,17 @@ sudo apt-mark hold xscreensaver
 
 sudo apt -y install \
   unattended-upgrades \
+  lxqt-admin \
   lxqt-core \
   lxqt-qtplugin \
   lxqt-config \
-  sddm \
+  lxqt-sudo \
+  sddm-theme-debian-maui \
   qterminal \
   pavucontrol-qt \
   open-vm-tools-desktop \
   snapd \
-  htop
+  htop \
+  gufw \
+  firefox \
+  thunderbird
