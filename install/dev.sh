47e56eeb5408ad51bf81da5cc9980b98bca2d9cd
tearne <tearne@gmail.com>
2020-01-26 14:48:33 +0000

    Using flatpack now

--- install/dev.sh
+++ install/dev.sh
@@ -3,19 +3,27 @@
 set -e
 DEBIAN_FRONTEND=noninteractive
 
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
+function install-sdk(){
+    sudo apt install -y zip unzip
+    curl -s "https://get.sdkman.io" | bash
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-sdk install java
-sdk install sbt
+    (sdk install java) || true
+    (sdk install sbt) || true
+}
+install-sdk
+######################################################
 
 sudo apt install -y snapd
 sudo snap install intellij-idea-community --classic
+######################################################
 
 function save-alias() {
+    # https://unix.stackexchange.com/questions/153977/automatically-put-an-alias-into-bashrc-or-zshrc
 
     ALIAS_NAME=`echo "$1" | grep -o ".*="`
     ALIASES_FILE_PATH=$HOME/.bash_aliases
+    touch $ALIASES_FILE_PATH
 
     # Deleting dublicate aliases
     sed -i "/alias $ALIAS_NAME/d" $ALIASES_FILE_PATH
@@ -28,26 +36,14 @@ function save-alias() {
     # Loading aliases
     source $ALIASES_FILE_PATH
 }
+######################################################
 
-
-function smartgit_old() {
-    VERSION=19_1_6
-
-    wget https://www.syntevo.com/downloads/smartgit/smartgit-$VERSION.deb -O /tmp/smartgit.deb
-    sudo apt install -y /tmp/smartgit.deb
-    rm /tmp/smartgit.deb
-
-    mkdir -p $HOME/.local/bin
-    ln -s /usr/share/smartgit/bin/smartgit.sh $HOME/.local/bin/smartgit
-    source $HOME/.profile
-}
-
-function smartgit_new() {
+function install-smartgit() {
     sudo apt install -y flatpak
     sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     sudo flatpak install -y flathub com.syntevo.SmartGit
 
     save-alise smartgit=flatpak run com.syntevo.SmartGit
 }
-
-smartgit_new
+install-smartgit
+######################################################
